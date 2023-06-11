89f25969460387aad807cd61c80a61efd1d84c09
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-11 09:05:47 +0300

    Update session.py

--- sbb_b/core/session.py
+++ sbb_b/core/session.py
@@ -7,7 +7,7 @@ from .bothseesion import bothseesion
 from .client import JmthonClient
 from .logger import logging
 
-LOGS = logging.getLogger("سورس سبايدر")
+LOGS = logging.getLogger("سورس حياه")
 __version__ = "2.10.6"
 
 loop = None
