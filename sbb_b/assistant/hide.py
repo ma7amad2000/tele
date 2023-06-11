cffad229354d1ca5a3be17b7a6541ec2519544db
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-11 09:02:02 +0300

    Update hide.py

--- sbb_b/assistant/hide.py
+++ sbb_b/assistant/hide.py
@@ -15,7 +15,7 @@ async def on_plug_in_callback_query_handler(event):
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "هذه الرسالة غير متوفرة الان في سيرفرات سبايدر"
+            reply_pop_up_alert = "هذه الرسالة غير متوفرة الان في سيرفرات حيــــاه"
     else:
         reply_pop_up_alert = "هذه الرسالة غير متوفرة"
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
