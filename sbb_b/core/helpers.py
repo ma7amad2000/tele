7180d76151624641c97729c47f6c19e8cf9da3ea
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-11 09:06:22 +0300

    Update helpers.py

--- sbb_b/core/helpers.py
+++ sbb_b/core/helpers.py
@@ -6,7 +6,7 @@ from telethon.utils import get_display_name
 
 from .events import NewMessage
 
-LOGGER = logging.getLogger("سورس سبايدر")
+LOGGER = logging.getLogger("سورس حياه")
 
 
 def printUser(entity: types.User) -> None:
