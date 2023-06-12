2aa281c231d5f2996ca066783a5a1077a704f792
الوسكي <126801003+ma7amad2000@users.noreply.github.com>
2023-06-12 10:04:17 +0300

    Update التحديث.py

--- sbb_b/plugins/التحديث.py
+++ sbb_b/plugins/التحديث.py
@@ -15,7 +15,6 @@ from sbb_b import HEROKU_APP, UPSTREAM_REPO_URL, sbb_b
 from ..Config import Config
 from ..core.logger import logging
 from ..core.managers import edit_delete, edit_or_reply
-from ..helpers.utils import _zedutils
 from ..sql_helper.global_collection import (
     add_to_collectionlist,
     del_keyword_collectionlist,
